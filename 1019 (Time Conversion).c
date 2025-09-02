72eef9a9dd745f2d063aea4186f32b2b2e2a0010
Natasa15-aaa <akazadctgdk@gmail.com>
2025-09-02 06:23:19 +0600

    Update 1019 (Time Conversion).c

--- 1019 (Time Conversion).c
+++ 1019 (Time Conversion).c
@@ -3,10 +3,12 @@
 int main() {
     int N, h, m, s;
     scanf("%d", &N);
+    
     h = N / 3600;
-    m = (N % 3600) / 60;
-    s = N % 60;
+ m = (N % 3600) / 60;
+ s = N % 60;
     printf("%d %d %d\n", h, m, s);
     return 0;
 }
+
 
