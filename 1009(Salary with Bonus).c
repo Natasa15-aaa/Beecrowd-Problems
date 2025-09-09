dac351b799ba8e6ab60e41481022eaf9b5c136f4
Natasa15-aaa <akazadctgdk@gmail.com>
2025-09-09 10:28:34 +0600

    Add files via upload

---
+++ 1009(Salary with Bonus).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    double salary, sales, total;
+
+    scanf("%s", name);
+    scanf("%lf %lf", &salary, &sales);
+
+    total = salary + sales * 0.15;
+
+    printf("TOTAL = R$ %.2lf\n", total);
+
+    return 0;
+}
+
